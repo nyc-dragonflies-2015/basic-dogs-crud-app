443d4ef932c93b6d6649b655c074a1a48f6ce697
Steven G. Harms <steven@devbootcamp.com>
2015-06-09 10:36:02 -0400

    Build out dogs routes further

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -4,3 +4,30 @@ get '/dogs' do
   @count = Dog.count
   erb :'dogs/index'
 end
+
+# "New"
+get '/dogs/new' do
+  erb :'dogs/new'
+end
+
+# "Show"
+get '/dogs/:name' do
+  @dog = Dog.find_by(name: params[:name])
+  erb :'dogs/show'
+end
+
+# "Create the new instance"
+post '/dogs' do
+  Dog.create(params[:dog])
+  redirect '/dogs'
+end
+
+# Display a form that we can use to alter an existing record
+get '/dogs/:id/edit' do
+end
+
+patch '/dogs/:id' do
+end
+
+delete '/dogs/:id' do
+end
