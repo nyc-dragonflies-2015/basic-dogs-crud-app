ea756c601737092e71589f48b8fbbe47f96ce92f
Steven G. Harms <steven@devbootcamp.com>
2015-06-09 10:03:30 -0400

    Define Dog seed instances

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,31 +1,10 @@
-zack = User.create(
-  first_name: 'Zack',
-  last_name: 'Mance',
-  age: 29,
-  email: 'zack@devbootcamp.com')
-chris = User.create(
-  first_name: 'Chris',
-  last_name: 'DiMartino',
-  age: 36,
-  email: 'chrisd@devbootcamp.com',
-  manager: zack)
+DOGS = [
+  ["Byron", "Poodle"],
+  ["Lassie", "Collie"],
+  ["Margaret", "Schnoodle"]
+]
 
-garden = Garden.create(name: 'NY Botanical')
-
-garden.users << zack << chris
-
-flowers = %w{Tulip Pansy Hydrangea Banana Rose}.map { |name| Flower.create(name: name) }
-
-flowers.each do |flower|
-  Planting.create(flower: flower, garden: garden, quantity: rand(40..100))
-end
-
-insects = [Bee, Fly, Mantis].map do |name|
-  insect = name.create
-  garden.insects << insect
-  insect
-end
-
-insects.each do |insect|
-  insect.preferred_flowers << flowers.sample
+DOGS.each do |dog|
+  Dog.create(name: dog.first, breed: dog.last)
+  puts "Created #{dog.first}"
 end
